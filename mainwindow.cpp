e5478f5df1db0b529af230c0540b1f987508304e
dyhnoah <dyhnoah@foxmail.com>
2021-01-26 20:21:55 +0800

    ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->fileButton, &QPushButton::clicked, this,[this](){
+        QString fileName = QFileDialog::getOpenFileName(this, "选择文件", "./");
+
+        ui->lineEdit->setText(fileName);
+    });
 }
 
 MainWindow::~MainWindow()
